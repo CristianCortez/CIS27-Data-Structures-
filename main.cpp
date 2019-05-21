046ec22a6ce1f963730eeede16d12616cdc6db54
SalvadorOchoaSanchez <49251110+SalvadorOchoaSanchez@users.noreply.github.com>
2019-05-21 13:37:59 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,147 +1,14 @@
-#include"lib.h"
+#include"Vertex.h"
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
 #include <stdio.h>
 #include <iostream>
+#include "movement.h"
+
+using namespace std;
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-
-Vertex vertices[] = {
-	//Position                            //Color                            //Texcoords
-
-	//Base
-	glm::vec3(-0.5f, 0.8f, 0.8f),          glm::vec3(1.f, 1.f, 1.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.5f, -0.8f, 0.8f),         glm::vec3(1.f, 1.f, 1.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.5f, -0.8f, 0.8f),          glm::vec3(1.f, 1.f, 1.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.5f, 0.8f, 0.8f),           glm::vec3(1.f, 1.f, 1.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	glm::vec3(-0.6f, 0.9f, 0.8f),          glm::vec3(0.5f, 1.f, 0.5f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.6f, -0.9f, 0.8f),         glm::vec3(0.5f, 1.f, 0.5f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.6f, -0.9f, 0.8f),          glm::vec3(0.5f, 1.f, 0.5f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.6f, 0.9f, 0.8f),           glm::vec3(0.5f, 1.f, 0.5f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 1
-	glm::vec3(-0.5f, 0.8f, 0.f),          glm::vec3(1.f, 1.f, 0.f),          glm::vec2(1.f, 1.f),     //Top left
-	glm::vec3(-0.5f, 0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(-0.25f, 0.16f, 0.f),        glm::vec3(0.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(-0.25f, 0.8f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 2
-	glm::vec3(-0.25f, 0.8f, 0.f),         glm::vec3(1.f, 0.f, 0.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.25f, 0.16f, 0.f),        glm::vec3(1.f, 0.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.25f, 0.16f, 0.f),         glm::vec3(1.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.25f, 0.8f, 0.f),          glm::vec3(1.f, 0.f, 0.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 3
-	glm::vec3(0.25f, 0.8f, 0.f),          glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(0.25f, 0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.5f, 0.16f, 0.f),          glm::vec3(0.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.5f, 0.8f, 0.f),           glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 4
-	glm::vec3(-0.5f, 0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.5f, -0.48f, 0.f),        glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(-0.25f, -0.48f, 0.f),       glm::vec3(0.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(-0.25f, 0.16f, 0.f),        glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 5
-	glm::vec3(-0.25f, 0.16f, 0.f),        glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.25f, -0.16f, 0.f),       glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.25f, -0.16f, 0.f),        glm::vec3(0.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.25f, 0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 6
-	glm::vec3(-0.25f, -0.16f, 0.f),       glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.25f, -0.48f, 0.f),       glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.0f, -0.48f, 0.f),         glm::vec3(0.f, 0.f, 0.f),         glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.0f, -0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 7
-	glm::vec3(0.0f, -0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(0.0f, -0.48f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.25f, -0.48f, 0.f),        glm::vec3(0.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.25f, -0.16f, 0.f),        glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 8
-	glm::vec3(0.25f, 0.16f, 0.f),         glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(0.25f, -0.48f, 0.f),        glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.5f, -0.48f, 0.f),         glm::vec3(0.f, 0.f, 0.f),         glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.5f, 0.16f, 0.f),          glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 9
-	glm::vec3(-0.5f, -0.48f, 0.f),        glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(-0.5f, -0.8f, 0.f),         glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(-0.25f, -0.8f, 0.f),        glm::vec3(0.f, 0.f, 0.f),         glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(-0.25f, -0.48f, 0.f),        glm::vec3(1.f, 1.f, 0.f),         glm::vec2(0.f, 0.f),     //Top right
-
-	//Block 10
-	glm::vec3(0.25f, -0.48f, 0.f),        glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 1.f),     //Top left
-	glm::vec3(0.25f, -0.8f, 0.f),         glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),     //Bottom left
-	glm::vec3(0.5f, -0.8f, 0.f),          glm::vec3(0.f, 0.f, 0.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.5f, -0.48f, 0.f),        glm::vec3(1.f, 1.f, 0.f),          glm::vec2(0.f, 0.f),      //Top right
-
-	//Exit
-	glm::vec3(-0.25f, -0.8f, 0.f),        glm::vec3(0.f, 0.f, 1.f),         glm::vec2(0.f, 0.f),    //Top left
-	glm::vec3(-0.25f, -0.9f, 0.f),        glm::vec3(0.f, 0.f, 1.f),         glm::vec2(1.f, 0.f),     //Bottom left
-	glm::vec3(0.25f, -0.9f, 0.f),          glm::vec3(0.f, 0.f, 1.f),          glm::vec2(1.f, 0.f),     //Bottom right
-	glm::vec3(0.25f, -0.8f, 0.f),           glm::vec3(0.f, 0.f, 1.f),          glm::vec2(0.f, 0.f)    //Top right
-};
-unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
-
-GLuint indices[] = {
-	//Block1
-	0, 1, 2,
-	0, 2, 3,
-
-	//Block 2
-	4, 5, 6,
-	4, 6, 7,
-
-	//Block 3
-	8, 9, 10,
-	8, 10, 11,
-
-	//Block 4
-	12, 13, 14,
-	12, 14, 15,
-
-	//Block 5
-	16, 17, 18,
-	16, 18, 19,
-
-	//Block 6
-	20, 21, 22,
-	20, 22, 23,
-
-	//Block 7
-	24, 25, 26,
-	24, 26, 27,
-
-	//Block 8
-	28, 29, 30,
-	28, 30, 31,
-
-	//Block 9
-	32, 33, 34,
-	32, 34, 35,
-
-	//Block 10
-	36, 37, 38,
-	36, 38, 39,
-
-	//Base:
-	40, 41, 42,
-	40, 42, 43,
-
-	//base 3:
-	44, 45, 46,
-	44, 46, 47,
-
-	48, 49, 50,
-	48, 50, 51
-};
-unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
 
 void updateInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -245,7 +112,95 @@ bool loadShaders(GLuint &program) {
 	return loadSuccess;
 }
 
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+
+	float x = ((2.0 / SCREEN_WIDTH) * xpos) - 1.0;
+	float y = (((2.0 / SCREEN_HEIGHT) * ypos) - 1.0) * -1.0;
+
+	cout << "Xpos: " << x << " Ypos: " << y << endl;
+
+}
+
+void keyPressed(unsigned char key, int x, int y) {
+
+	
+
+	oldkeyPressReset = key;
+	oldkeyPressUndo = key;
+
+}
+
+void keyUp(unsigned char key, int x, int y) {
+
+	if (key == 'r') {
+
+		oldkeyPressReset = key
+
+	}
+
+	else if (key == 'backspace') {
+
+		oldkeyPressUndo = key;
+
+	}
+
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+
+	if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+
+		keyPressReset == GLFW_PRESS;
+
+	}
+	else if (key == GLFW_KEY_R && action == GLFW_RELEASE) {
+
+		keyPressUndo == GLFW_RELEASE;
+
+	}
+	else if (key == GLFW_KEY_BACKSPACE && action == GLFW_PRESS) {
+
+		keyPressUndo == GLFW_PRESS;
+
+	}
+	else if (key == GLFW_KEY_BACKSPACE && action == GLFW_RELEASE) {
+
+		keyPressUndo == GLFW_RELEASE;
+
+	}
+
+
+
+}
+
+
 int main() {
+
+	Vertex vertices[52];
+	initVertexAry(vertices);
+	unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
+
+	GLuint indices[78];
+	initIndicies(indicies);
+	unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
+
+	//MOUSE START
+	int stateMouse;
+	int oldStateMouse = GLFW_RELEASE;
+	int keyPressReset;
+	int oldkeyPressReset = GLFW_RELEASE;
+	int keyPressUndo;
+	int oldkeyPressUndo = GLFW_RELEASE;
+	int indexOfFirstPositionVecOfSelectedBlock;
+	double xposStart;
+	double yposStart;
+	double xposEnd;
+	double yposEnd;
+	int direction;
+	int winFlag;
+
+	//MOUSE END
 
 	//INIT GLFW
 	glfwInit();
@@ -279,7 +234,7 @@ int main() {
 	//ERROR
 	if (glewInit() != GLEW_OK) {
 		std::cout << "ERROR::MAIN.CPP::GLEW_INIT_FAILED" << "\n";
-		glfwTerminate;
+		glfwTerminate();
 	}
 
 
@@ -336,10 +291,260 @@ int main() {
 	glBindVertexArray(0);
 
 
+
+
 	//MAIN LOOP
 	while (!glfwWindowShouldClose(window)) {
 
-                glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+		
+
+		// MOUSE START
+
+		glfwSetCursorPosCallback(window, cursor_position_callback);
+
+
+
+		stateMouse = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+
+		if (stateMouse == GLFW_PRESS && oldStateMouse == GLFW_RELEASE) {
+
+			glfwGetCursorPos(window, &xposStart, &yposStart);
+
+			xposStart = ((2.0 / SCREEN_WIDTH) * xposStart) - 1.0;
+			yposStart = (((2.0 / SCREEN_HEIGHT) * yposStart) - 1.0) * -1.0;
+
+			indexOfFirstPositionVecOfSelectedBlock = selectBlock(xposStart, yposStart, vertices);
+
+			cout << "  SELECTED BLOCK: " << indexOfFirstPositionVecOfSelectedBlock;
+
+		}
+
+		else if(stateMouse == GLFW_RELEASE && oldStateMouse == GLFW_PRESS) {
+		
+			glfwGetCursorPos(window, &xposEnd, &yposEnd);
+
+			xposEnd = ((2.0 / SCREEN_WIDTH) * xposEnd) - 1.0;
+			yposEnd = (((2.0 / SCREEN_HEIGHT) * yposEnd) - 1.0) * -1.0;
+
+			direction = getDirection(xposEnd, yposEnd, indexOfFirstPositionVecOfSelectedBlock, vertices);
+
+			if (isSpaceEmpty(direction, xposEnd, yposEnd, indexOfFirstPositionVecOfSelectedBlock, vertices)) {
+
+				cout << "  SELECTED BLOCK: " << indexOfFirstPositionVecOfSelectedBlock;
+
+				cout << "  Direction: " << direction;
+
+				switch (direction) {
+
+				case 1:
+
+					moveLeft(indexOfFirstPositionVecOfSelectedBlock, vertices);
+
+					//push(vertices[indexOfFirstPositionVecOfSelectedBlock], 
+					//	vertices[indexOfFirstPositionVecOfSelectedBlock + 1], 
+					//	vertices[indexOfFirstPositionVecOfSelectedBlock + 2], 
+					//	vertices[indexOfFirstPositionVecOfSelectedBlock + 3])
+
+					// Vertex Array Object
+					glGenVertexArrays(1, &VAO);
+					glBindVertexArray(VAO);
+
+					//Gen VAO AND BIND
+					glGenBuffers(1, &VBO);
+					glBindBuffer(GL_ARRAY_BUFFER, VBO);
+					glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+					//GEN EBO AND BIND AND SEND DATA
+					glGenBuffers(1, &EBO);
+					glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+					glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+					//SET VERTEXATTRIBPOINTERS AND ENABLE (INPUT ASSEMBLY)
+					//Position
+					glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+					glEnableVertexAttribArray(0);
+					//Color
+					glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+					glEnableVertexAttribArray(1);
+					//Texcoord
+					glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
+					glEnableVertexAttribArray(2);
+
+					//BIND VAO 0
+					glBindVertexArray(0);
+
+					break;
+
+				case 2:
+
+					moveRight(indexOfFirstPositionVecOfSelectedBlock, vertices);
+
+					// Vertex Array Object
+					glGenVertexArrays(1, &VAO);
+					glBindVertexArray(VAO);
+
+
+					//Gen VAO AND BIND
+					glGenBuffers(1, &VBO);
+					glBindBuffer(GL_ARRAY_BUFFER, VBO);
+					glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+					//GEN EBO AND BIND AND SEND DATA
+					glGenBuffers(1, &EBO);
+					glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+					glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+					//SET VERTEXATTRIBPOINTERS AND ENABLE (INPUT ASSEMBLY)
+					//Position
+					glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+					glEnableVertexAttribArray(0);
+					//Color
+					glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+					glEnableVertexAttribArray(1);
+					//Texcoord
+					glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
+					glEnableVertexAttribArray(2);
+
+					//BIND VAO 0
+					glBindVertexArray(0);
+
+					break;
+
+				case 3:
+
+					cout << "   DID IT CHANGE :" << vertices[indexOfFirstPositionVecOfSelectedBlock].position[1];
+
+					moveUp(indexOfFirstPositionVecOfSelectedBlock, vertices);
+
+					cout << "   DID IT CHANGE :" << vertices[indexOfFirstPositionVecOfSelectedBlock].position[1];
+
+					// Vertex Array Object
+					glGenVertexArrays(1, &VAO);
+					glBindVertexArray(VAO);
+
+
+					//Gen VAO AND BIND
+					glGenBuffers(1, &VBO);
+					glBindBuffer(GL_ARRAY_BUFFER, VBO);
+					glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+					//GEN EBO AND BIND AND SEND DATA
+					glGenBuffers(1, &EBO);
+					glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+					glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+					//SET VERTEXATTRIBPOINTERS AND ENABLE (INPUT ASSEMBLY)
+					//Position
+					glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+					glEnableVertexAttribArray(0);
+					//Color
+					glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+					glEnableVertexAttribArray(1);
+					//Texcoord
+					glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
+					glEnableVertexAttribArray(2);
+
+					//BIND VAO 0
+					glBindVertexArray(0);
+
+					break;
+
+				case 4:
+
+					moveDown(indexOfFirstPositionVecOfSelectedBlock, vertices);
+
+					// Vertex Array Object
+					glGenVertexArrays(1, &VAO);
+					glBindVertexArray(VAO);
+
+
+					//Gen VAO AND BIND
+					glGenBuffers(1, &VBO);
+					glBindBuffer(GL_ARRAY_BUFFER, VBO);
+					glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+					//GEN EBO AND BIND AND SEND DATA
+					glGenBuffers(1, &EBO);
+					glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+					glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+					//SET VERTEXATTRIBPOINTERS AND ENABLE (INPUT ASSEMBLY)
+					//Position
+					glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+					glEnableVertexAttribArray(0);
+					//Color
+					glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+					glEnableVertexAttribArray(1);
+					//Texcoord
+					glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
+					glEnableVertexAttribArray(2);
+
+					//BIND VAO 0
+					glBindVertexArray(0);
+
+					break;
+
+				default:
+
+					cout << "You shouldn't be here";
+
+					break;
+
+				}
+			}
+
+			indexOfFirstPositionVecOfSelectedBlock = -1;
+			xposStart = -99.0;
+			yposStart = -99.0;
+			xposEnd = -99.0;
+			yposEnd = -99.0;
+			direction = -1;
+
+		}
+		
+		oldStateMouse = stateMouse;
+
+		//MOUSE END
+
+
+
+
+		winFlag = winCondition(vertices); //DO SOMETHING WITH THIS
+
+
+
+
+
+		glfwSetKeyCallback(window, key_callback);
+
+		
+
+		if (keyPressReset == GLFW_PRESS && oldkeyPressReset == GLFW_RELEASE) {
+
+			initVertexAry(vertices);
+			cout << "RESET";
+
+		}
+
+		else if (keyPressUndo == GLFW_PRESS && oldkeyPressUndo == GLFW_RELEASE) {
+
+			//UNDO
+
+			cout << "BACKSPACE";
+
+		}
+
+		oldkeyPressReset = keyPressReset;
+
+		oldkeyPressUndo = keyPressUndo;
+
+
+        glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
